95ead3d48c0a104170245759c5c32a98408e0b94
AmineAmrii <amine.amri.2021@gmail.com>
2022-08-05 18:04:36 +0100

    update project

--- src/test/java/com/e2etests/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2etests/automation/RunWebSuiteTest.java
@@ -7,13 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = {"src/spec/features"},
-				//plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"},
-						plugin = {"pretty", "json:target/cucumber.json"},
-		tags = ("@patient"),
-		monochrome = true,
-		snippets = CAMELCASE)
+@CucumberOptions(features = { "src/spec/features" }, plugin = { "pretty", "html:target/cucumber-report.html",
+		"json:target/cucumber.json" }, tags = ("@patient"), monochrome = true, snippets = CAMELCASE)
 
 public class RunWebSuiteTest {
 
